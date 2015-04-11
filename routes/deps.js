1db8388db4ff3c2d1e32ff0f059b53dd0e2a68f4
Gabor Csardi <csardi.gabor@gmail.com>
2015-04-11 19:24:20 -0400

    Support Suggests, #1, return format changed completely

--- routes/deps.js
+++ routes/deps.js
@@ -12,15 +12,19 @@ var base_packages = ["base", "compiler", "datasets", "graphics",
 
 var essential_dep_types = [ "Depends", "LinkingTo", "Imports" ];
 var optional_dep_types = [ "Suggests", "Enhances" ];
-var all_dep_types = essential_dep_types + optional_dep_types;
+var all_dep_types = essential_dep_types.concat(optional_dep_types);
+var dep_types = { 'ess': essential_dep_types,
+		  'all': all_dep_types,
+		  'allall': all_dep_types };
 
-var re_pkgs = '\\/([-\\w0-9\\.,]+)';
+var re_pkgs = '(\\/(all))?\\/([-\\w0-9\\.,]+)';
 var re_full = new RegExp('^' + re_pkgs + '$');
 
 router.get(re_full, function(req, res) {
-    var pkgs = parse_packages(req.params[0])
+    var which = req.params[1] || "ess"
+    var pkgs = parse_packages(req.params[2])
     res.set('Content-Type', 'application/json');
-    do_query(res, pkgs);
+    do_query(res, pkgs, which);
 });
 
 function parse_packages(pkgs) {
@@ -31,7 +35,7 @@ function parse_packages(pkgs) {
     return pkgs
 }
 
-function do_query(res, pkgs) {
+function do_query(res, pkgs, which) {
     var pkgs_str = pkgs.map(function(x) {
 	return(x.join("-"))
     })
@@ -39,25 +43,27 @@ function do_query(res, pkgs) {
     request(url, function (error, response, body) {
 	if (error || response.statusCode != 200) { return handle_error(res); }
 	var pkg_obj = JSON.parse(body);
-	do_package(res, pkg_obj)
+	do_package(res, pkg_obj, which)
     })
 }
 
-function get_deps(pkg_obj) {
-    var deps = [];
-    for (d in essential_dep_types) {
-	var dd = essential_dep_types[d];
-	deps = deps.concat(Object.keys(pkg_obj[dd] || []));
+function get_deps(pkg_obj, which) {
+    var my_dep_types = dep_types[which];
+    var deps = {};
+    for (d in my_dep_types) {
+	var dd = my_dep_types[d];
+	var new_deps = pkg_obj[dd] || { };
+	deps[dd] = new_deps;
     }
-    var rindex = deps.indexOf('R');
-    if (rindex > -1) deps.splice(rindex, 1);
     return deps;
 }
 
-function do_package(res, pkg_obj) {
+function do_package(res, pkg_obj, which) {
 
     var deps = {} 		// Contains the results
     var seen = { false: false }	// Keep track of what was queried
+
+    var which2 = which == "allall" ? "allall" : "ess"
 
     var cranq = async.queue(function(task, callback) {
 	if (base_packages.indexOf(task) > -1) {
@@ -69,17 +75,19 @@ function do_package(res, pkg_obj) {
 	    request(url, function(error, response, body) {
 		if (error || response.statusCode != 200) { return handle_error(res); }
 		var pkg_obj = JSON.parse(body)
-		var task_deps = get_deps(pkg_obj)
+		var task_deps = get_deps(pkg_obj, which2)
 		var ver = pkg_obj["Version"]
 		var pkg_ver = task + '-' + ver
 		deps[pkg_ver] = task_deps
 		seen[task] = pkg_ver
-		task_deps.map(function(x) {
-		    if (! (x in seen)) {
-			seen[x] = false
-			cranq.push(x)
+		for (var key in task_deps) {
+		    for (var pkg in task_deps[key]) {
+			if (pkg != 'R' && ! (pkg in seen)) {
+			    seen[pkg] = false
+			    cranq.push(pkg)
+			}
 		    }
-		})
+		}
 		callback();
 	    })
 	}
@@ -89,12 +97,19 @@ function do_package(res, pkg_obj) {
 
     for (i in pkg_obj) {
 	var pkg = pkg_obj[i]
-	var pkg_deps = get_deps(pkg)
+	var pkg_deps = get_deps(pkg, which)
 	var pkg_name = pkg["Package"]
 	var pkg_ver = pkg["Version"]
 	seen[pkg_name] = pkg_name + '-' + pkg_ver
 	deps[pkg_name + '-' + pkg_ver] = pkg_deps
-	pkg_deps.map(function(x) { seen[x] = false; cranq.push(x) })
+	for (var key in pkg_deps) {
+	    for (var pkg in pkg_deps[key]) {
+		if (pkg != 'R') {
+		    seen[pkg] = false;
+		    cranq.push(pkg);
+		}
+	    }
+	}
     }
 
     // In case there are no dependencies. The plus one is for false: false.
@@ -107,12 +122,18 @@ function return_res(res, deps, seen) {
     var keys = Object.keys(deps);
 
     // Need to add version numbers
-    Object.keys(deps).forEach(function(key) {
-	var value = deps[key]
-	if (value !== false) {
-	    deps[key] = value.map(function(y) { return seen[y] })
+    for (pkg in deps) {
+	for (deptype in deps[pkg]) {
+	    for (deppkg in deps[pkg][deptype]) {
+		if (deppkg != 'R') {
+		    var deppkgver = seen[deppkg];
+		    deps[pkg][deptype][deppkgver] = deps[pkg][deptype][deppkg];
+		    delete deps[pkg][deptype][deppkg];
+		}
+	    }
 	}
-    })
+    }
+
     res.set(200)
     res.send(JSON.stringify(deps))
     res.end()
